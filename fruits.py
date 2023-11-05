b98f0cbbab44e9522658c66db69a1bb21757dbe9
Simon Wickman <79363190+Simbomb@users.noreply.github.com>
2023-11-05 18:16:31 +0100

    Update fruits.py

--- fruits.py
+++ fruits.py
@@ -2,142 +2,135 @@ import pygame as pg
 from physics import *
 
 size_ratio = 8
-friction = 0.0
+friction = 0.1
+damping = 0.2
 class Fruit:
-    def __init__(self, x, y, size, color):
-        self.x = x
-        self.y = y
+    def __init__(self, pos, size, color):
+        self.pos = np.array(pos)
         self.size = size
         self.color = color
-        self.momentum = np.array([0, 0], dtype='float64')
+        self.momentum = np.array([0, 0])
+        self.mass = size*10
+        self.gravity = self.mass*1
     
     def draw(self, screen):
         #Draw the fruit
-        pg.draw.circle(screen, self.color, (self.x, self.y), self.size)
+        pg.draw.circle(screen, self.color, (self.pos[0], self.pos[1]), self.size)
 
-    def update(self):
-        self.momentum *= (1-friction)
-        self.x += self.momentum[0]
-        self.y += self.momentum[1]
+    def move(self, height, width):
+        self.pos = self.pos + self.momentum*damping
+        self.momentum = self.momentum * (1-friction)
+        x, y = self.pos
+        #Check boundaries
+        if y >= height-self.size:
+            self.pos[1] = height-self.size
+            self.momentum[1] = min(0, -self.momentum[1])
+        if x <= self.size:
+            self.pos[0] = self.size
+            self.momentum[0] = max(0, -self.momentum[0])
+        if x >= width-self.size:
+            self.pos[0] = width-self.size
+            self.momentum[0] = min(0, -self.momentum[0])
+        if y <= self.size:
+            self.pos[1] = self.size
+            self.momentum[1] = max(0, -self.momentum[1])
+    
+    def distance(self, fruit):
+        return np.linalg.norm(self.pos - fruit.pos)
+    
+    def touch(self, fruit):
+        return self.distance(fruit) <= self.size + fruit.size
 
-    def gravity(self, current_fruits):
-        if self.y + self.size < 1000:
-            no_touch = True
-            for fruit in current_fruits:
-                if fruit is not self:
-                    collision, direction_vector, distance, momentum_vector = collision_detection(self, fruit)
-                    #Touch
-                    if collision:
-                        
-                        no_touch = False
-                        self.momentum += momentum_vector
-                        fruit.momentum -= momentum_vector
-                        #Move fruit
-                        self.x += direction_vector[0]
-                        self.y += direction_vector[1]
-
-                        #Upgrade fruits
-                        if self.type == fruit.type:
-                            current_fruits.append(self.next(fruit))
-                            current_fruits.remove(self)
-                            current_fruits.remove(fruit)
-
-            #Gravity
-            if no_touch:
-                self.momentum[1] = 0.1
-        self.update()
-        
-        
-
+    def updateX(self, x):
+        self.pos[0] = x
 
 class Cherry(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 1*size_ratio, (210, 4, 45))
+        super().__init__([x, y], 1*size_ratio, (210, 4, 45))
         self.type = "cherry"
     
     def next(self, fruit):
-        return Strawberry(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Strawberry(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Strawberry(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 2*size_ratio, (200, 63, 73))
+        super().__init__([x, y], 2*size_ratio, (200, 63, 73))
         self.type = "apple"
     
     def next(self, fruit):
-        return Grape(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Grape(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Grape(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 3*size_ratio, (111, 45, 168))
+        super().__init__([x, y], 3*size_ratio, (111, 45, 168))
         self.type = "grape"
     
     def next(self, fruit):
-        return Dekopon(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Dekopon(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Dekopon(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 4*size_ratio, (240,152,77))
+        super().__init__([x, y], 4*size_ratio, (240,152,77))
         self.type = "dekopon"
     
     def next(self, fruit):
-        return Orange(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Orange(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Orange(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 5*size_ratio, (240, 133, 45))
+        super().__init__([x, y], 5*size_ratio, (240, 133, 45))
         self.type = "orange"
     
     def next(self, fruit):
-        return Apple(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Apple(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Apple(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 6*size_ratio, (200, 55, 47))
+        super().__init__([x, y], 6*size_ratio, (200, 55, 47))
         self.type = "apple"
     
     def next(self, fruit):
-        return Pear(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Pear(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Pear(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 7*size_ratio, (82, 89, 19))
+        super().__init__([x, y], 7*size_ratio, (82, 89, 19))
         self.type = "pear"
     
     def next(self, fruit):
-        return Peach(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Peach(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Peach(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 8*size_ratio, (255, 203, 164))
+        super().__init__([x, y], 8*size_ratio, (255, 203, 164))
         self.type = "peach"
     
     def next(self, fruit):
-        return Pineapple(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Pineapple(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Pineapple(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 9*size_ratio, (255, 234, 99))
+        super().__init__([x, y], 9*size_ratio, (255, 234, 99))
         self.type = "pineapple"
     
     def next(self, fruit):
-        return Melon(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Melon(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 class Melon(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 10*size_ratio, (187,246,150))
+        super().__init__([x, y], 10*size_ratio, (187,246,150))
         self.type = "melon"
     
     def next(self, fruit):
-        return Watermelon(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
+        return Watermelon(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 
 class Watermelon(Fruit):
     def __init__(self, x, y):
-        super().__init__(x, y, 11*size_ratio, (0, 47, 27))
+        super().__init__([x, y], 11*size_ratio, (0, 47, 27))
         self.type = "Watermelon"
     
     def next(self, fruit):
-        return Cherry(abs(self.x + fruit.x) / 2, abs(self.y + fruit.y) / 2)
-
+        return Cherry(abs(self.pos[0] + fruit.pos[0]) / 2, abs(self.pos[1] + fruit.pos[1]) / 2)
 
 
