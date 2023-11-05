df30e0dc6e10bef09174bfa53f64225eee2cf3ff
Simon Wickman <79363190+Simbomb@users.noreply.github.com>
2023-11-05 18:16:42 +0100

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -18,38 +18,38 @@ space_pressed = False
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("Suika Game")
 x, y = SCREEN_WIDTH // 2, 0
-
+DELAY = 20
+random_fruit_class = random.choice(fruit_classes)
+new_fruit = random_fruit_class(x, y)
 running = True
 while running:
+    pg.time.delay(DELAY)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
     # Handle user input
     keys = pg.key.get_pressed()
-    x += (keys[pg.K_RIGHT] - keys[pg.K_LEFT])
+    x += (keys[pg.K_RIGHT] - keys[pg.K_LEFT])*DELAY
     #y += (keys[pg.K_DOWN] - keys[pg.K_UP])
     if keys[pg.K_SPACE] and not space_pressed:
+        current_fruits.append(new_fruit)
         random_fruit_class = random.choice(fruit_classes)
         new_fruit = random_fruit_class(x, y)
-        current_fruits.append(new_fruit)
         space_pressed = True
     if not keys[pg.K_SPACE]:
             space_pressed = False
 
     screen.fill((0, 0, 0))
+    new_fruit.updateX(x)
+    new_fruit.draw(screen)
 
-
-    #Go through each fruits functions
-    current_fruits.sort(key=lambda fruit: fruit.y)
-    for fruit in current_fruits:     
-        fruit.gravity(current_fruits)
-        fruit.x = max(fruit.size, min(SCREEN_WIDTH - fruit.size, fruit.x))
-        fruit.y = max(fruit.size, min(SCREEN_HEIGHT - fruit.size, fruit.y))
+    for fruit in current_fruits:
+        fruit.move(SCREEN_HEIGHT, SCREEN_WIDTH) 
         fruit.draw(screen)
 
-
-
+    handle_collision(current_fruits)
+    #handle_overlap(current_fruits)
 
     pg.display.flip()
 
